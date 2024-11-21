19ea6192a661a7ed7cf9f5eac544bad21dc2bbab
Ravi Sharma Gautam <ravi.sharmagautam@defradev.onmicrosoft.com>
2024-11-21 10:17:17 +0000

    Added testfunction to test the function deployment

---
+++ src/EprPrnIntegration.Api/TestFunction.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace EprPrnIntegration.Api
+{
+    public class TestFunction
+    {
+        private readonly ILogger<TestFunction> _logger;
+
+        public TestFunction(ILogger<TestFunction> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("TestFunction")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
