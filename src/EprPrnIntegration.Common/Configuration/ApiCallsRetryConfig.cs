15110e1512a943148249b65770bb6cd0152e1739
H Yadav <himanshu.yadav.external@eviden.com>
2024-12-10 13:20:49 +0000

    updated settings.json and added retry config

---
+++ src/EprPrnIntegration.Common/Configuration/ApiCallsRetryConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace EprPrnIntegration.Common.Configuration;
+
+[ExcludeFromCodeCoverage]
+public class ApiCallsRetryConfig
+{
+    public int? WaitTimeBetweenRetryInSecs { get; set; }
+    public int? MaxAttempts { get; set; }
+}
