f55e415c6d02ab7aa30d91fe80bf0c88a9703cef
H Yadav <himanshu.yadav.external@eviden.com>
2024-12-20 16:03:22 +0000

    470280: config setting update

--- src/EprPrnIntegration.Common/Configuration/MessagingConfig.cs
+++ src/EprPrnIntegration.Common/Configuration/MessagingConfig.cs
@@ -8,7 +8,7 @@ public class MessagingConfig
     public const string SectionName = "MessagingConfig";
 
     public string? PrnTemplateId { get; set; }
-    public string? PERNTemplateId { get; set; }
+    public string? PernTemplateId { get; set; }
     public string? NpwdEmailTemplateId { get; set; }
     public string? NpwdEmail { get; set; }
     public string? ApiKey { get; set; }
