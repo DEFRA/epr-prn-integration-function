9a1f5fd6b8d04e60327581ab9a197f371c5ad357
H Yadav <himanshu.yadav.external@eviden.com>
2025-01-24 13:46:00 +0000

    466225: added model for updated org recon

---
+++ src/EprPrnIntegration.Common/Models/Npwd/UpdatedOrganisationReconciliationSummary.cs
@@ -0,0 +1,9 @@
+﻿namespace EprPrnIntegration.Common.Models.Npwd;
+
+public class UpdatedOrganisationReconciliationSummary
+{
+    public string? Name { get; set; } = null;
+    public string? Id { get; set; } = null;
+    public string? Address { get; set; } = null;
+    public string? Date { get; set; } = null;
+}
