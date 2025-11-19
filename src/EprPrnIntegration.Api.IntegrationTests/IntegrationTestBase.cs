d9c8903e40a6dbb4f8a475515ccb38cc58342b40
Tim Peel <tim.peel@equalexperts.com>
2025-11-19 14:29:52 +0000

    Add initial integration test project and example tests

---
+++ src/EprPrnIntegration.Api.IntegrationTests/IntegrationTestBase.cs
@@ -0,0 +1,7 @@
+using Xunit;
+
+namespace EprPrnIntegration.Api.IntegrationTests;
+
+[Trait("Category", "IntegrationTest")]
+[Collection("Integration Tests")]
+public abstract class IntegrationTestBase;
