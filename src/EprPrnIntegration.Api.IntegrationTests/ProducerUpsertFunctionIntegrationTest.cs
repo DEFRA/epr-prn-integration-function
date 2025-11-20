9b0d20dc038aadb25e40f055b6d4237144b8a1a4
Dan Sweeting <dansweeting@gmail.com>
2025-11-20 15:56:19 +0000

    refactor: extension methods

--- src/EprPrnIntegration.Api.IntegrationTests/ProducerUpsertFunctionIntegrationTest.cs
+++ src/EprPrnIntegration.Api.IntegrationTests/ProducerUpsertFunctionIntegrationTest.cs
@@ -1,6 +1,3 @@
-using System.Net;
-using WireMock.Admin.Mappings;
-using WireMock.Client.Extensions;
 using Xunit;
 
 namespace EprPrnIntegration.Api.IntegrationTests;
@@ -26,61 +23,17 @@ public class ProducerUpsertFunctionIntegrationTest : IntegrationTestBase, IAsync
     [Fact]
     public async Task WhenAzureFunctionIsInvoked_SendsUpdatedProducerToNPWD()
     {
-        await Given_CommonDataAPIHasUpdateFor("Acme Manufacturing Ltd");
+        await _wireMockContext.CommonDataApiHasUpdateFor("Acme Manufacturing Ltd");
 
-        await Given_NpwdAcceptsProducerPatch();
+        await _wireMockContext.NpwdAcceptsProducerPatch();
 
         await _azureFunctionInvokerContext.InvokeAzureFunction(FunctionName.UpdateProducersList);
 
         await AsyncWaiter.WaitForAsync(async () =>
         {
-            var requestsModel = new RequestModel { Methods = ["PATCH"], Path = "/odata/producers" };
-            var requests = await _wireMockContext.WireMockAdminApi.FindRequestsAsync(requestsModel);
+            var requests = await _wireMockContext.GetNpwdProducersPatchRequests();
 
             Assert.Contains(requests, entry => entry.Request.Body!.Contains("Acme Manufacturing Ltd"));
         });
-    }
-
-    private async Task Given_NpwdAcceptsProducerPatch()
-    {
-        var mappingBuilder = _wireMockContext.WireMockAdminApi.GetMappingBuilder();
-        mappingBuilder.Given(builder =>
-            builder.WithRequest(request => request.UsingPatch().WithPath("/odata/producers"))
-                .WithResponse(response => response.WithStatusCode(HttpStatusCode.Accepted))
-        );
-        var status = await mappingBuilder.BuildAndPostAsync();
-        Assert.NotNull(status.Guid);
-    }
-
-    private async Task Given_CommonDataAPIHasUpdateFor(string name)
-    {
-        var mappingBuilder = _wireMockContext.WireMockAdminApi.GetMappingBuilder();
-        mappingBuilder.Given(builder =>
-            builder.WithRequest(request => request.UsingGet().WithPath("/api/producer-details/get-updated-producers"))
-                .WithResponse(response => response.WithStatusCode(HttpStatusCode.OK).WithBodyAsJson(new[]
-                {
-                    new
-                    {
-                        organisationName = name,
-                        tradingName = "Acme Plastics",
-                        organisationType = "Limited Company",
-                        companiesHouseNumber = "12345678",
-                        organisationId = "ORG-001",
-                        addressLine1 = "123 Industrial Estate",
-                        addressLine2 = "Unit 5",
-                        town = "Manchester",
-                        county = "Greater Manchester",
-                        country = "England",
-                        postcode = "M1 1AA",
-                        peprid = "EPR-123456",
-                        status = "Active",
-                        businessCountry = "United Kingdom",
-                        updatedDateTime = "2025-01-15T10:30:00Z"
-                    }
-                }))
-        );
-
-        var status = await mappingBuilder.BuildAndPostAsync();
-        Assert.NotNull(status.Guid);
     }
 }
