776a5401054bb84bc2ed6e26d637564085915187
Tim Peel <tim.peel@equalexperts.com>
2025-11-21 11:08:50 +0000

    Feature: Custom health check for use when infra can change (#120)
    * Add simple health check that allows anonymous to be used as health check monitor
    
    * Change port to match one used in launch settings so Docker or dotnet run of function is interchangable
    
    * Refactor function invoker context as it only needs to initialise once
    
    * Add simple integration test for health check
    
    * Add shared accepted spellings
    
    * Refactor existing integration tests
    
    * Refactor test names and location
    
    * Change to assertion for Sonar
    
    * Ignore code coverage as covered in integration tests

--- src/EprPrnIntegration.Api.IntegrationTests/IntegrationTestBase.cs
+++ src/EprPrnIntegration.Api.IntegrationTests/IntegrationTestBase.cs
@@ -4,4 +4,19 @@ namespace EprPrnIntegration.Api.IntegrationTests;
 
 [Trait("Category", "IntegrationTest")]
 [Collection("Integration Tests")]
-public abstract class IntegrationTestBase;
+public abstract class IntegrationTestBase : IAsyncLifetime
+{
+    protected WireMockContext WireMockContext = null!;
+
+    public async Task InitializeAsync()
+    {
+        WireMockContext = new WireMockContext();
+        
+        await WireMockContext.InitializeAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await WireMockContext.DisposeAsync();
+    }
+}
