cd9da29aa45c3a61604a305cbba501b1f0255075
Dan Sweeting <dansweeting@gmail.com>
2025-11-25 14:33:41 +0000

    feature/REEX 19 add stub RREPW client (#125)
    * chore: adds typing for RREPW PATCH endpoint
    
    * feat: mapper to generate RREPW endpoint payload
    
    * silence, SONARQUBE
    
    * feat: stub RREPW
    
    * removes comment noise
    
    * fix: remove PII from log entry

---
+++ src/EprPrnIntegration.Common/Models/Rrepw/ProducerUpdateRequest.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+
+namespace EprPrnIntegration.Common.Models.Rrepw;
+
+public class ProducerUpdateRequest
+{
+    [JsonProperty("id")]
+    public required string Id { get; set; }
+
+    [JsonProperty("name")]
+    public required string Name { get; set; }
+
+    [JsonProperty("tradingName")]
+    public string? TradingName { get; set; }
+
+    [JsonProperty("type")]
+    public ProducerType? Type { get; set; } 
+
+    [JsonProperty("status")]
+    public ProducerStatus? Status { get; set; }
+
+    [JsonProperty("addressLine1")]
+    public string? AddressLine1 { get; set; }
+
+    [JsonProperty("addressLine2")]
+    public string? AddressLine2 { get; set; }
+
+    [JsonProperty("town")]
+    public string? Town { get; set; }
+
+    [JsonProperty("county")]
+    public string? County { get; set; }
+
+    [JsonProperty("country")]
+    public string? Country { get; set; }
+
+    [JsonProperty("postcode")]
+    public string? Postcode { get; set; }
+}
