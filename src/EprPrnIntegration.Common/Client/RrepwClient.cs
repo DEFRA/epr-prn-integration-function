cb931b1d98d7821404f7fa1dc6bc7b8bf010d77a
Dan Sweeting <dansweeting@gmail.com>
2025-11-25 11:33:29 +0000

    feat: stub RREPW

---
+++ src/EprPrnIntegration.Common/Client/RrepwClient.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using EprPrnIntegration.Common.Models.Rrepw;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace EprPrnIntegration.Common.Client;
+
+// A temporary stub for RREPW's producer endpoint
+[ExcludeFromCodeCoverage]
+public class RrepwClient(
+    ILogger<RrepwClient> logger)
+{
+    public Task Patch(ProducerUpdateRequest request)
+    {
+        var json = JsonConvert.SerializeObject(request);
+        
+        logger.LogInformation("Patching producer update to RREPW: ${json}", json);
+        
+        return Task.CompletedTask;
+    } 
+}
