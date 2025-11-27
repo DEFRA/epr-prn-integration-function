b3907a8966720ad622094edebf0f4cd9408c0cc5
Dan Sweeting <dansweeting@gmail.com>
2025-11-27 09:16:45 +0000

    feat: cloned method on common-data-api client to get from new intended endpoint (#128)

--- src/EprPrnIntegration.Common/RESTServices/CommonService/Interfaces/ICommonDataService.cs
+++ src/EprPrnIntegration.Common/RESTServices/CommonService/Interfaces/ICommonDataService.cs
@@ -5,5 +5,6 @@ namespace EprPrnIntegration.Common.RESTServices.CommonService.Interfaces
     public interface ICommonDataService
     {
         Task<List<UpdatedProducersResponse>> GetUpdatedProducers(DateTime from, DateTime to, CancellationToken cancellationToken);
+        Task<List<UpdatedProducersResponseV2>> GetUpdatedProducersV2(DateTime from, DateTime to, CancellationToken cancellationToken);
     }
 }
