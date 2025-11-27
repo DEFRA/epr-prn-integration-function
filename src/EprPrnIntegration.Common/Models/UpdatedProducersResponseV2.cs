10644b0dca1ecc2d72cfce83b4c67d4cd385e1e1
Dan Sweeting <dansweeting@gmail.com>
2025-11-27 09:16:45 +0000

    feat: cloned method on common-data-api client to get from new intended endpoint (#128)

---
+++ src/EprPrnIntegration.Common/Models/UpdatedProducersResponseV2.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace EprPrnIntegration.Common.Models;
+
+[ExcludeFromCodeCoverage]
+public class UpdatedProducersResponseV2
+{
+    public string? OrganisationName { get; set; }
+    public string? TradingName { get; set; }
+    public string? OrganisationType { get; set; }
+    public string? CompaniesHouseNumber { get; set; }
+    public string? OrganisationId { get; set; }
+    public string? AddressLine1 { get; set; }
+    public string? AddressLine2 { get; set; }
+    public string? Town { get; set; }
+    public string? County { get; set; }
+    public string? Country { get; set; }
+    public string? Postcode { get; set; }
+    public string? PEPRID { get; set; }
+    public string? Status { get; set; }
+    public string? BusinessCountry { get; set; }
+    public DateTime? UpdatedDateTime { get; set; }
+    public int SubmissionYear { get; set; }
+}
