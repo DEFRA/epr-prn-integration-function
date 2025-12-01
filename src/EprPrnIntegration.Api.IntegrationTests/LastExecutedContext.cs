b0631e916c2bf453b858d07b81c83f44eba46f11
Dan Sweeting <dansweeting@gmail.com>
2025-12-01 09:36:39 +0000

    removes OTT way to get azure connection string

--- src/EprPrnIntegration.Api.IntegrationTests/LastExecutedContext.cs
+++ src/EprPrnIntegration.Api.IntegrationTests/LastExecutedContext.cs
@@ -10,7 +10,10 @@ public static class LastExecutedContext
 
     static LastExecutedContext()
     {
-        var connectionString = DockerEnvironmentHelper.GetAzureWebJobsStorage();
+        // This connection string is the well-known default credential for the Azurite storage emulator.
+        // It is NOT a sensitive secret - it's a hardcoded value built into Azurite for local development.
+        // See: https://learn.microsoft.com/en-us/azure/storage/common/storage-use-azurite#well-known-storage-account-and-key
+        var connectionString = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1;QueueEndpoint=http://localhost:10001/devstoreaccount1;TableEndpoint=http://localhost:10002/devstoreaccount1;";
         var blobServiceClient = new BlobServiceClient(connectionString);
         var blobStorage = new BlobStorage(blobServiceClient);
         LastUpdateService = new LastUpdateService(blobStorage);
