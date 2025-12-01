2091fa4e2bdf560353fee73f23e1af1f971cc280
Dan Sweeting <dansweeting@gmail.com>
2025-12-01 14:49:45 +0000

    chore: type updates for waste-organisation-api (#129)
    * chore: renames types to reference Waste-Organisation api
    
    * chore: passing the v2 type into the mapper
    
    * rewrites mapping code inline with new schema
    
    * refactor: separate switch statements for separate fields
    
    * chore: updates to latest agreed assumptions
    
    * chore: removes unused types
    
    * removes nullable on org year
    
    * updates DTO to match common-data-api
    
    * Revert "updates DTO to match common-data-api"
    
    This reverts commit 1672068e7ada9a5b55f34396ae1012c2e2107407.
    
    * renames

---
+++ src/EprPrnIntegration.Common.UnitTests/Mappers/WasteOrganisationsApiUpdateRequestMapperTests.cs
@@ -0,0 +1,175 @@
+﻿using EprPrnIntegration.Common.Mappers;
+using EprPrnIntegration.Common.Models;
+using EprPrnIntegration.Common.Models.WasteOrganisationsApi;
+using FluentAssertions;
+
+namespace EprPrnIntegration.Common.UnitTests.Mappers
+{
+    public class WasteOrganisationsApiUpdateRequestMapperTests
+    {
+        [Fact]
+        public void MapsTopLevelFields()
+        {
+            var input = new UpdatedProducersResponseV2
+            {
+                PEPRID = Guid.NewGuid().ToString(),
+                
+                AddressLine1 = "address1",
+                AddressLine2 = "address2",
+                Town = "town",
+                County = "county",
+                Postcode = "postcode",
+                Country = "UK",
+                
+                CompaniesHouseNumber = "some-company-number",
+                OrganisationName = "Organisation's Name",
+                TradingName = "Organisation's TradingName",
+                
+                SubmissionYear = 2025,
+                Status = "registered",
+                OrganisationType = "DP"
+            };
+            
+            var result = WasteOrganisationsApiUpdateRequestMapper.Map(input);
+            
+            result.Id.Should().Be(input.PEPRID);
+            result.Name.Should().Be("Organisation's Name");
+            result.TradingName.Should().Be("Organisation's TradingName");
+            result.CompaniesHouseNumber.Should().Be("some-company-number");
+        }
+
+        [Fact]
+        public void MapsAddress()
+        {
+            var input = new UpdatedProducersResponseV2
+            {
+                PEPRID = Guid.NewGuid().ToString(),
+                
+                AddressLine1 = "address1",
+                AddressLine2 = "address2",
+                Town = "town",
+                County = "county",
+                Postcode = "postcode",
+                Country = "UK",
+                
+                
+                Status = "registered",
+                OrganisationType = "DP",
+                SubmissionYear = 2025,
+                
+                CompaniesHouseNumber = "some-company-number",
+                OrganisationName = "Organisation's Name",
+                TradingName = "Organisation's TradingName"
+            };
+            
+            var result = WasteOrganisationsApiUpdateRequestMapper.Map(input);
+
+            result.Address.Should().BeEquivalentTo(new Address
+            {
+                AddressLine1 = "address1",
+                AddressLine2 = "address2",
+                Town = "town",
+                County = "county",
+                Postcode = "postcode",
+                Country = "UK",
+            });
+        }
+
+        [Theory]
+        [InlineData("England", BusinessCountry.England)]
+        [InlineData("Northern Ireland", BusinessCountry.NorthernIreland)]
+        [InlineData("Wales", BusinessCountry.Wales)]
+        [InlineData("Scotland", BusinessCountry.Scotland)]
+        [InlineData("", null)]
+        public void MapsBusinessCountry(string country, BusinessCountry? expectedBusinessCountry)
+        {
+            var producer = new UpdatedProducersResponseV2
+            {
+                PEPRID = Guid.NewGuid().ToString(),
+                OrganisationName = Guid.NewGuid().ToString(),
+                Status = "registered",
+                OrganisationType = "DP",
+                SubmissionYear = 2026,
+                BusinessCountry = country
+            };
+        
+            var result = WasteOrganisationsApiUpdateRequestMapper.Map(producer);
+            
+            result.BusinessCountry.Should().Be(expectedBusinessCountry);
+        }
+
+        [Theory]
+        [InlineData("registered", "DP", RegistrationStatus.Registered, RegistrationType.LargeProducer)]
+        [InlineData("deleted", "DP", RegistrationStatus.Cancelled, RegistrationType.LargeProducer)]
+        [InlineData("registered", "S", RegistrationStatus.Registered, RegistrationType.ComplianceScheme)]
+        [InlineData("deleted", "S", RegistrationStatus.Cancelled, RegistrationType.ComplianceScheme)]
+        public void MapsRegistration(string status, string orgType, RegistrationStatus expectedStatus, RegistrationType expectedRegistrationType)
+        {
+            var producer = new UpdatedProducersResponseV2
+            {
+                PEPRID = Guid.NewGuid().ToString(),
+                OrganisationName = Guid.NewGuid().ToString(),
+                Status = status,
+                OrganisationType = orgType,
+                SubmissionYear = 2026
+            };
+        
+            var result = WasteOrganisationsApiUpdateRequestMapper.Map(producer);
+            
+            result.Registration.Should().BeEquivalentTo(new Registration
+            {
+               Status = expectedStatus,
+               Type = expectedRegistrationType,
+               SubmissionYear = 2026,
+            });
+        }
+
+        [Fact]
+        public void ThrowsForUnrecognisedStatus()
+        {
+            Assert.Throws<ArgumentException>(() => WasteOrganisationsApiUpdateRequestMapper.Map(new UpdatedProducersResponseV2
+            {
+                PEPRID = Guid.NewGuid().ToString(),
+                OrganisationName = Guid.NewGuid().ToString(),
+                Status = "foobar",
+                OrganisationType = "DP",
+                SubmissionYear = 2026
+            }));
+        }
+        
+        [Fact]
+        public void ThrowsForUnrecognisedOrganisationType()
+        {
+            Assert.Throws<ArgumentException>(() => WasteOrganisationsApiUpdateRequestMapper.Map(new UpdatedProducersResponseV2
+            {
+                PEPRID = Guid.NewGuid().ToString(),
+                OrganisationName = Guid.NewGuid().ToString(),
+                Status = "registered",
+                OrganisationType = "foobar",
+                SubmissionYear = 2026
+            }));
+        }
+
+        [Fact]
+        public void ThrowsForMissingId()
+        {
+            var producer = new UpdatedProducersResponseV2
+            {
+                OrganisationName = "some name"
+            };
+            
+            Assert.Throws<ArgumentException>(() => WasteOrganisationsApiUpdateRequestMapper.Map(producer));
+        }
+        
+        [Fact]
+        public void ThrowsForMissingOrganisationName()
+        {
+            var producer = new UpdatedProducersResponseV2
+            {
+                PEPRID = Guid.NewGuid().ToString(),
+            };
+            
+            Assert.Throws<ArgumentException>(() => WasteOrganisationsApiUpdateRequestMapper.Map(producer));
+        }
+    }
+}
---
+++ src/EprPrnIntegration.Common/Models/WasteOrganisationsApi/WasteOrganisationsApiUpdateRequest.cs
@@ -0,0 +1,107 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace EprPrnIntegration.Common.Models.WasteOrganisationsApi;
+
+public class WasteOrganisationsApiUpdateRequest
+{
+    [JsonProperty("id")]
+    public required string Id { get; set; }
+
+    [JsonProperty("name")]
+    public required string Name { get; set; }
+
+    [JsonProperty("tradingName")]
+    public string? TradingName { get; set; }
+
+    [JsonProperty("businessCountry")]
+    public BusinessCountry? BusinessCountry { get; set; }
+
+    [JsonProperty("companiesHouseNumber")]
+    public string? CompaniesHouseNumber { get; set; }
+
+    [JsonProperty("address")]
+    public required Address Address { get; set; }
+
+    [JsonProperty("registration")]
+    public required Registration Registration { get; set; }
+}
+
+public class Address
+{
+    [JsonProperty("addressLine1")]
+    public string? AddressLine1 { get; set; }
+
+    [JsonProperty("addressLine2")]
+    public string? AddressLine2 { get; set; }
+
+    [JsonProperty("postcode")]
+    public string? Postcode { get; set; }
+
+    [JsonProperty("country")]
+    public string? Country { get; set; }
+
+    [JsonProperty("county")]
+    public string? County { get; set; }
+
+    [JsonProperty("town")]
+    public string? Town { get; set; }
+}
+
+public class Registration
+{
+    [JsonProperty("status")]
+    public required RegistrationStatus Status { get; set; }
+
+    [JsonProperty("type")]
+    public required RegistrationType Type { get; set; }
+
+    [JsonProperty("submissionYear")]
+    public required int SubmissionYear { get; set; }
+}
+
+[JsonConverter(typeof(StringEnumConverter))]
+public enum RegistrationStatus
+{
+    [EnumMember(Value = "REGISTERED")]
+    Registered,
+
+    [EnumMember(Value = "CANCELLED")]
+    Cancelled
+}
+
+[JsonConverter(typeof(StringEnumConverter))]
+public enum RegistrationType
+{
+    [EnumMember(Value = "SMALL-PRODUCER")]
+    SmallProducer,
+
+    [EnumMember(Value = "LARGE-PRODUCER")]
+    LargeProducer,
+
+    [EnumMember(Value = "COMPLIANCE-SCHEME")]
+    ComplianceScheme,
+
+    [EnumMember(Value = "REPROCESSOR")]
+    Reprocessor,
+
+    [EnumMember(Value = "EXPORTER")]
+    Exporter
+}
+
+[JsonConverter(typeof(StringEnumConverter))]
+public enum BusinessCountry
+{
+    [EnumMember(Value = "GB-ENG")]
+    England,
+
+    [EnumMember(Value = "GB-NIR")]
+    NorthernIreland,
+
+    [EnumMember(Value = "GB-SCT")]
+    Scotland,
+
+    [EnumMember(Value = "GB-WLS")]
+    Wales
+}
