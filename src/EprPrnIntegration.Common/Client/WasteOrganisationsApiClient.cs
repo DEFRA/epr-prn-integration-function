8fae27102ba42916463373ae890768f819acb9ed
Dan Sweeting <dansweeting@gmail.com>
2025-12-01 14:49:45 +0000

    chore: type updates for waste-organisation-api (#129)
    * chore: renames types to reference Waste-Organisation api
    
    * chore: passing the v2 type into the mapper
    
    * rewrites mapping code inline with new schema
    
    * refactor: separate switch statements for separate fields
    
    * chore: updates to latest agreed assumptions
    
    * chore: removes unused types
    
    * removes nullable on org year
    
    * updates DTO to match common-data-api
    
    * Revert "updates DTO to match common-data-api"
    
    This reverts commit 1672068e7ada9a5b55f34396ae1012c2e2107407.
    
    * renames

---
+++ src/EprPrnIntegration.Common/Client/WasteOrganisationsApiClient.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using EprPrnIntegration.Common.Models.WasteOrganisationsApi;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace EprPrnIntegration.Common.Client;
+
+// A temporary stub for the waste-organisations-api endpoint
+[ExcludeFromCodeCoverage]
+public class WasteOrganisationsApiClient(
+    ILogger<WasteOrganisationsApiClient> logger)
+{
+    public Task Patch(WasteOrganisationsApiUpdateRequest request)
+    {
+        var json = JsonConvert.SerializeObject(new {
+           id = request.Id,
+           registration = new
+           {
+               status = request.Registration.Status,
+               type = request.Registration.Type,
+           },
+           businessCountry = request.BusinessCountry,
+        });
+        
+        logger.LogInformation("Patching producer update to RREPW: ${json}", json);
+        
+        return Task.CompletedTask;
+    } 
+}
---
+++ src/EprPrnIntegration.Common/Mappers/WasteOrganisationsApiUpdateRequestMapper.cs
@@ -0,0 +1,80 @@
+using EprPrnIntegration.Common.Models;
+using EprPrnIntegration.Common.Models.WasteOrganisationsApi;
+
+namespace EprPrnIntegration.Common.Mappers;
+
+public static class WasteOrganisationsApiUpdateRequestMapper
+{
+    public static WasteOrganisationsApiUpdateRequest Map(UpdatedProducersResponseV2 updatedProducer)
+    {
+        if (updatedProducer.PEPRID == null)
+        {
+            throw new ArgumentException("PEPRID is null");
+        }
+        
+        if (updatedProducer.OrganisationName == null)
+        {
+            throw new ArgumentException("OrganisationName is null");
+        }
+        
+        return new WasteOrganisationsApiUpdateRequest
+        {
+            Id = updatedProducer.PEPRID,
+            Name = updatedProducer.OrganisationName,
+            TradingName = updatedProducer.TradingName,
+            CompaniesHouseNumber = updatedProducer.CompaniesHouseNumber,
+            BusinessCountry = MapBusinessCountry(updatedProducer.BusinessCountry),
+            Registration = MapRegistration(updatedProducer),
+            Address = MapAddress(updatedProducer),
+        };
+    }
+
+    private static Address MapAddress(UpdatedProducersResponseV2 updatedProducer)
+    {
+        return new Address
+        {
+            AddressLine1 = updatedProducer.AddressLine1,
+            AddressLine2 = updatedProducer.AddressLine2,
+            Town = updatedProducer.Town,
+            County = updatedProducer.County,
+            Postcode = updatedProducer.Postcode,
+            Country = updatedProducer.Country
+        };
+    }
+
+    private static BusinessCountry? MapBusinessCountry(string? businessCountry)
+    {
+        return (businessCountry) switch
+        {
+            "England" => BusinessCountry.England,
+            "Northern Ireland" => BusinessCountry.NorthernIreland,
+            "Scotland" => BusinessCountry.Scotland,
+            "Wales" => BusinessCountry.Wales,
+            _ => null
+        };
+    }
+
+    private static Registration MapRegistration(UpdatedProducersResponseV2 updatedProducer)
+    {
+        var type = (updatedProducer.OrganisationType) switch
+        {
+            "DP" => RegistrationType.LargeProducer,
+            "S" => RegistrationType.ComplianceScheme,
+            _ => throw new ArgumentException($"Unknown registration type {updatedProducer.OrganisationType}")
+        };
+
+        var status = (updatedProducer.Status) switch
+        {
+            "registered" => RegistrationStatus.Registered,
+            "deleted" => RegistrationStatus.Cancelled,
+            _ => throw new ArgumentException($"Unknown status {updatedProducer.Status}")
+        };
+
+        return new Registration
+        {
+            Status = status,
+            Type = type,
+            SubmissionYear = updatedProducer.SubmissionYear
+        };
+    }
+}
