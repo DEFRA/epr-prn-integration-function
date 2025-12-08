21aad83af3fd7b49e32137864387821dc38090e0
Dan Sweeting <dansweeting@gmail.com>
2025-12-08 17:37:25 +0000

    adds integration test for transient errors in the waste organisations api

--- src/EprPrnIntegration.Api.IntegrationTests/Functions/UpdateWasteOrganisationsTests.cs
+++ src/EprPrnIntegration.Api.IntegrationTests/Functions/UpdateWasteOrganisationsTests.cs
@@ -39,7 +39,7 @@ public class UpdateWasteOrganisationsTests : IntegrationTestBase
     }
     
     [Fact]
-    public async Task WhenAzureFunctionIsInvoked_UpdatesLastUpdatedTimestamp()
+    public async Task WhenAzureFunctionIsInvoked_With_UpdatesFound_UpdatesLastUpdatedTimestamp()
     {
         var id = await CommonDataApiStub.HasV2UpdateFor("acme");
         await WasteOrganisationsApiStub.AcceptsOrganisation(id);
@@ -70,6 +70,28 @@ public class UpdateWasteOrganisationsTests : IntegrationTestBase
 
             entries.Count.Should().Be(1);
             entries[0].Request.Body!.Should().Contain("acme-resilient");
+        });
+    }
+
+    [Fact]
+    public async Task WhenWasteOrganisationsApiHasTransientFailure_RetriesAndEventuallySucceedsAndUpdatesLastUpdated()
+    {
+        var id = await CommonDataApiStub.HasV2UpdateFor("acme-transient");
+        await WasteOrganisationsApiStub.AcceptsOrganisationWithTransientFailures(id);
+
+        var before = await LastUpdateService.GetLastUpdate("UpdateWasteOrganisations") ?? DateTime.MinValue;
+
+        await AzureFunctionInvokerContext.InvokeAzureFunction(FunctionName.UpdateWasteOrganisations);
+
+        await AsyncWaiter.WaitForAsync(async () =>
+        {
+            var entries = await WasteOrganisationsApiStub.GetOrganisationRequests(id);
+
+            entries.Count.Should().BeGreaterOrEqualTo(1, "request should eventually succeed after retry");
+            entries[0].Request.Body!.Should().Contain("acme-transient");
+
+            var after = await LastUpdateService.GetLastUpdate("UpdateWasteOrganisations");
+            after.Should().BeAfter(before);
         });
     }
 }
--- src/EprPrnIntegration.Api.IntegrationTests/Stubs/WasteOrganisationsApi.cs
+++ src/EprPrnIntegration.Api.IntegrationTests/Stubs/WasteOrganisationsApi.cs
@@ -24,4 +24,31 @@ public class WasteOrganisationsApi(WireMockContext wireMock)
         var requestsModel = new RequestModel { Methods = ["PUT"], Path = $"/organisations/{id}/" };
         return await wireMock.WireMockAdminApi.FindRequestsAsync(requestsModel);
     }
+
+    public async Task AcceptsOrganisationWithTransientFailures(string id)
+    {
+        var scenarioName = "WasteOrgTransientFailure-" + Guid.NewGuid();
+
+        // First mapping: return 503 and transition to "Attempt1" state
+        var failureMapping = wireMock.WireMockAdminApi.GetMappingBuilder();
+        failureMapping.Given(builder =>
+            builder.WithRequest(request => request.UsingPut().WithPath($"/organisations/{id}/"))
+                .WithResponse(response => response.WithStatusCode(HttpStatusCode.ServiceUnavailable))
+                .WithScenario(scenarioName)
+                .WithSetStateTo("Attempt1")
+        );
+        var failureMappingStatus = await failureMapping.BuildAndPostAsync();
+        Assert.NotNull(failureMappingStatus.Guid);
+
+        // Second mapping: return 202 Accepted when in "Attempt1" state
+        var successMapping = wireMock.WireMockAdminApi.GetMappingBuilder();
+        successMapping.Given(builder =>
+            builder.WithRequest(request => request.UsingPut().WithPath($"/organisations/{id}/"))
+                .WithResponse(response => response.WithStatusCode(HttpStatusCode.Accepted))
+                .WithScenario(scenarioName)
+                .WithWhenStateIs("Attempt1")
+        );
+        var successMappingStatus = await successMapping.BuildAndPostAsync();
+        Assert.NotNull(successMappingStatus.Guid);
+    }
 }
