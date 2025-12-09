beb49c16361d4e6a53e94f4aa11f1f4b1a9dabbc
Dan Sweeting <dansweeting@gmail.com>
2025-12-09 15:29:37 +0000

    stub cognito in integration tests

---
+++ src/EprPrnIntegration.Api.IntegrationTests/Stubs/CognitoApi.cs
@@ -0,0 +1,34 @@
+using System.Net;
+using WireMock.Client.Extensions;
+using Xunit;
+
+namespace EprPrnIntegration.Api.IntegrationTests.Stubs;
+
+public class CognitoApi(WireMockContext wireMock)
+{
+    public async Task<string> SetupOAuthTokenEndpoint()
+    {
+        var accessToken = $"test-bearer-token-{Guid.NewGuid()}";
+
+        var mappingBuilder = wireMock.WireMockAdminApi.GetMappingBuilder();
+        mappingBuilder.Given(builder =>
+            builder.WithRequest(request => request
+                    .UsingPost()
+                    .WithPath("/cognito/oauth/token")
+                    .WithHeader("Content-Type", "application/x-www-form-urlencoded")
+                )
+                .WithResponse(response => response
+                    .WithStatusCode(HttpStatusCode.OK)
+                    .WithBodyAsJson(new
+                    {
+                        access_token = accessToken,
+                        token_type = "Bearer",
+                        expires_in = 3600
+                    }))
+        );
+        var status = await mappingBuilder.BuildAndPostAsync();
+        Assert.NotNull(status.Guid);
+
+        return accessToken;
+    }
+}
