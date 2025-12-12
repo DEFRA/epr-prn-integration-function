7e437c1d694de8739c0cdb3938d93c24a639b5bb
Dan Sweeting <dansweeting@gmail.com>
2025-12-12 18:51:04 +0000

    replaces deleted file

---
+++ src/EprPrnIntegration.Common.UnitTests/NpwdOAuthMiddlewareTests.cs
@@ -0,0 +1,12 @@
+namespace EprPrnIntegration.Common.UnitTests
+{
+    public class NpwdOAuthMiddlewareTests
+    {
+        [Fact]
+        public void Test_Middleware_Appends_Bearer_Token_To_Requests_Correctly()
+        {
+            // TO DO:
+            Assert.True(true);
+        }
+    }
+}
