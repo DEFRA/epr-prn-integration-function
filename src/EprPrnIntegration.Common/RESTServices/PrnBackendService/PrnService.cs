a931701311bec8ba806993e0bc4d023e07600b74
Tim Rutter <tim.rutter@devsectr.com>
2025-12-12 15:34:31 +0000

    Return [] when null

--- src/EprPrnIntegration.Common/RESTServices/PrnBackendService/PrnService.cs
+++ src/EprPrnIntegration.Common/RESTServices/PrnBackendService/PrnService.cs
@@ -72,7 +72,7 @@ public class PrnService : BaseHttpService, IPrnService
      
         _logger.LogInformation("Getting Reconciled updated PRN's for date range from {FromDate} to {ToDate}", fromDate, toDate);
 
-        return await Get<List<ReconcileUpdatedPrnsResponseModel>>($"syncstatuses?from={fromDate}&to={toDate}", 
-            CancellationToken.None, false);
+        return await Get<List<ReconcileUpdatedPrnsResponseModel>?>($"syncstatuses?from={fromDate}&to={toDate}", 
+            CancellationToken.None, false) ?? [];
     }
 }
