8b260430e12200151df1a2cf748fdd6cc9e875c5
Dan Sweeting <dansweeting@gmail.com>
2025-12-17 16:19:00 +0000

    chore: REEX-150: adds placeholder for new PRN endpoint with DTO ready to be mapped (#149)
    * chore: REEX-150: adds placeholder for new PRN endpoint with DTOs ready to be mapped
    
    * remove concretions
    
    * Update with latest changes to common api
    
    ---------
    
    Co-authored-by: Tim Rutter <tim.rutter@devsectr.com>

---
+++ src/EprPrnIntegration.Common/Models/SavePrnDetailsRequestV2.cs
@@ -0,0 +1,28 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace EprPrnIntegration.Common.Models;
+
+[ExcludeFromCodeCoverage]
+public class SavePrnDetailsRequestV2
+{
+    public required string SourceSystemId { get; set; }
+    public required string PrnNumber { get; set; } 
+    public required int PrnStatusId { get; set; }
+    public required string PrnSignatory { get; set; }
+    public string? PrnSignatoryPosition { get; set; }
+    public required DateTime StatusUpdatedOn { get; set; }
+    public required string IssuedByOrg { get; set; }
+    public required Guid OrganisationId { get; set; }
+    public required string OrganisationName { get; set; }
+    public required string AccreditationNumber { get; set; }
+    public required string AccreditationYear { get; set; }
+    public required string MaterialName { get; set; }
+    public required string ReprocessorExporterAgency { get; set; }
+    public string? ReprocessingSite { get; set; }
+    public required bool DecemberWaste { get; set; }
+    public required bool IsExport { get; set; }
+    public required int TonnageValue { get; set; }
+    public string? IssuerNotes { get; set; }
+    public required string ProcessToBeUsed { get; set; }
+    public required string ObligationYear { get; set; }
+}
