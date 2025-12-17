85561d6c7362179da7b8512fc8ee4bbb3e14e9ae
Tim Rutter <tim.rutter@devsectr.com>
2025-12-17 14:49:47 +0000

    Updates

--- src/EprPrnIntegration.Common/Models/SavePrnDetailsRequestV2.cs
+++ src/EprPrnIntegration.Common/Models/SavePrnDetailsRequestV2.cs
@@ -10,17 +10,18 @@ public class SavePrnDetailsRequestV2
     public required int PrnStatusId { get; set; }
     public required string PrnSignatory { get; set; }
     public string? PrnSignatoryPosition { get; set; }
-    public DateTime? StatusUpdatedOn { get; set; }
+    public required DateTime StatusUpdatedOn { get; set; }
     public required string IssuedByOrg { get; set; }
     public required Guid OrganisationId { get; set; }
     public required string OrganisationName { get; set; }
     public required string AccreditationNumber { get; set; }
     public required string AccreditationYear { get; set; }
-    public required string MaterialName { get; set; } = "";
+    public required string MaterialName { get; set; }
     public required string ReprocessorExporterAgency { get; set; }
     public string? ReprocessingSite { get; set; }
     public required bool DecemberWaste { get; set; }
     public required bool IsExport { get; set; }
     public required int TonnageValue { get; set; }
     public string? IssuerNotes { get; set; }
+    public required string ProcessToBeUsed { get; set; }
 }
