0c6f3f85c17cb94ce712d0bf1d0a98d5bb4bc62e
Tim Rutter <tim.rutter@devsectr.com>
2025-12-17 11:59:27 +0000

    Further updates following review

--- src/EprPrnIntegration.Common/Models/SavePrnDetailsRequestV2.cs
+++ src/EprPrnIntegration.Common/Models/SavePrnDetailsRequestV2.cs
@@ -6,7 +6,7 @@ namespace EprPrnIntegration.Common.Models;
 public class SavePrnDetailsRequestV2
 {
     public required string SourceSystemId { get; set; }
-    public string PrnNumber { get; set; } = "";
+    public required string PrnNumber { get; set; } 
     public required int PrnStatusId { get; set; }
     public required string PrnSignatory { get; set; }
     public string? PrnSignatoryPosition { get; set; }
@@ -16,7 +16,7 @@ public class SavePrnDetailsRequestV2
     public required string OrganisationName { get; set; }
     public required string AccreditationNumber { get; set; }
     public required string AccreditationYear { get; set; }
-    public string MaterialName { get; set; } = "";
+    public required string MaterialName { get; set; } = "";
     public required string ReprocessorExporterAgency { get; set; }
     public required string ReprocessingSite { get; set; }
     public required bool DecemberWaste { get; set; }
