68d6645d7a9fc49227ee89586ea1caff4cac6bd1
Dan Sweeting <dansweeting@gmail.com>
2025-12-17 16:31:11 +0000

    removes any logic from the mapper, just return default stuff

--- src/EprPrnIntegration.Common/Mappers/PackagingRecyclingNoteToSavePrnDetailsRequestMapper.cs
+++ src/EprPrnIntegration.Common/Mappers/PackagingRecyclingNoteToSavePrnDetailsRequestMapper.cs
@@ -1,4 +1,5 @@
 using EprPrnIntegration.Common.Constants;
+using EprPrnIntegration.Common.Enums;
 using EprPrnIntegration.Common.Helpers;
 using EprPrnIntegration.Common.Models;
 using EprPrnIntegration.Common.Models.Rrepw;
@@ -11,58 +12,35 @@ public static class PackagingRecyclingNoteToSavePrnDetailsRequestMapper
 {
     public static SavePrnDetailsRequest Map(PackagingRecyclingNote prn, IConfiguration config, ILogger logger)
     {
-        var resolvedYear = ObligationYearResolver.GetDefaultObligationYear(config, logger);
-
         return new SavePrnDetailsRequest
         {
-            AccreditationNo = prn.Accreditation.AccreditationNumber,
-            AccreditationYear = prn.Accreditation.AccreditationYear.ToString(),
-            CancelledDate = prn.Status.CancelledAt,
-            DecemberWaste = prn.IsDecemberWaste,
-            EvidenceMaterial = prn.Accreditation.Material,
-            EvidenceNo = prn.PrnNumber,
-            EvidenceStatusCode = RrepwStatusToPrnStatusMapper.Map(prn.Status.CurrentStatus),
-            EvidenceTonnes = prn.TonnageValue,
-            IssueDate = prn.Status.AuthorisedAt,
-            IssuedByNPWDCode = ParseGuid(prn.IssuedByOrganisation.Id),
-            IssuedByOrgName = prn.IssuedByOrganisation.Name,
-            IssuedToNPWDCode = ParseGuid(prn.IssuedToOrganisation.Id),
-            IssuedToOrgName = prn.IssuedToOrganisation.Name,
-            IssuerNotes = prn.IssuerNotes,
-            StatusDate = prn.Status.AuthorisedAt ?? prn.Status.AcceptedAt ?? prn.Status.RejectedAt ?? prn.Status.CancelledAt,
+            AccreditationNo = "DUMMY_ACCREDITATION",
+            AccreditationYear = "2024",
+            CancelledDate = null,
+            DecemberWaste = false,
+            EvidenceMaterial = "DUMMY_MATERIAL",
+            EvidenceNo = "DUMMY_EVIDENCE_NO",
+            EvidenceStatusCode = EprnStatus.ACCEPTED,
+            EvidenceTonnes = 0,
+            IssueDate = DateTime.UtcNow,
+            IssuedByNPWDCode = Guid.Empty,
+            IssuedByOrgName = "DUMMY_ISSUED_BY_ORG",
+            IssuedToNPWDCode = Guid.Empty,
+            IssuedToOrgName = "DUMMY_ISSUED_TO_ORG",
+            IssuerNotes = "DUMMY_NOTES",
+            StatusDate = DateTime.UtcNow,
             CreatedByUser = "IntegrationFA",
             SourceSystemId = "RREPW",
-            // Not available in PackagingRecyclingNote
             IssuedToEPRId = null,
-            IssuerRef = "",
+            IssuerRef = "DUMMY_REF",
             MaterialOperationCode = null,
             ModifiedOn = null,
-            ObligationYear = resolvedYear,
+            ObligationYear = "2024",
             PrnSignatory = null,
             PrnSignatoryPosition = null,
             ProducerAgency = null,
             RecoveryProcessCode = null,
             ReprocessorAgency = null
         };
-    }
-
-    private static Guid? ParseGuid(string? input)
-    {
-        if (Guid.TryParse(input, out var guid))
-        {
-            return guid;
-        }
-        return null;
-    }
-
-    public static bool IsExport(string evidenceNo)
-    {
-        if (string.IsNullOrEmpty(evidenceNo))
-            return false;
-
-        var val = evidenceNo.Substring(0, 2).Trim();
-
-        return string.Equals(val, ExporterCodePrefixes.EaExport, StringComparison.InvariantCultureIgnoreCase)
-               || string.Equals(val, ExporterCodePrefixes.SepaExport, StringComparison.InvariantCultureIgnoreCase);
     }
 }
