9d32809723f6cb512bcef16e2bcf8055c6ba5217
Dan Sweeting <dansweeting@gmail.com>
2025-12-18 17:26:09 +0000

    removes logging

--- src/EprPrnIntegration.Common/RESTServices/WasteOrganisationsService/WasteOrganisationsService.cs
+++ src/EprPrnIntegration.Common/RESTServices/WasteOrganisationsService/WasteOrganisationsService.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics.CodeAnalysis;
-using System.Text.Json;
 using EprPrnIntegration.Common.Configuration;
 using EprPrnIntegration.Common.Constants;
 using EprPrnIntegration.Common.Models.WasteOrganisationsApi;
@@ -24,22 +23,9 @@ namespace EprPrnIntegration.Common.RESTServices.WasteOrganisationsService
             HttpClientNames.WasteOrganisations,
             config.Value.TimeoutSeconds), IWasteOrganisationsService
     {
-        private readonly ILogger<WasteOrganisationsService> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-
         public async Task UpdateOrganisation(string id, WasteOrganisationsApiUpdateRequest organisation)
         {
-            LogPayload(id, organisation);
-
             await Put($"{id}", organisation, CancellationToken.None);
-        }
-
-        // This function is temporary for testing, since we don't yet have a working instance of the service.
-        // This MUST be removed before promoting to PRE prod,
-        // since it runs the risk of leaking PII with production-like data.
-        private void LogPayload(string id, WasteOrganisationsApiUpdateRequest organisation)
-        {
-            var json = JsonSerializer.Serialize(organisation, new JsonSerializerOptions { WriteIndented = true });
-            _logger.LogInformation("Sending organisation update for {OrganisationId}:\n{Json}", id, json);
         }
     }
 }
