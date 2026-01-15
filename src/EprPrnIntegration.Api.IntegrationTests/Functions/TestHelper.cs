0a4990eba0a1366287885cd592d856fb8163ff02
Tim Rutter <tim.rutter@devsectr.com>
2026-01-15 13:27:35 +0000

    namespace

--- src/EprPrnIntegration.Api.IntegrationTests/Functions/TestHelper.cs
+++ src/EprPrnIntegration.Api.IntegrationTests/Functions/TestHelper.cs
@@ -2,19 +2,22 @@ using EprPrnIntegration.Api.IntegrationTests.Stubs;
 using EprPrnIntegration.Common.Models.Rrepw;
 using EprPrnIntegration.Common.Models.WasteOrganisationsApi;
 
-public static class TestHelper
+namespace EprPrnIntegration.Api.IntegrationTests.Functions
 {
-    public static async Task SetupOrganisations(
-        List<PackagingRecyclingNote> prns,
-        CognitoApi cognitoApiStub,
-        WasteOrganisationsApi WasteOrganisationsApiStub
-    )
+    public static class TestHelper
     {
-        await cognitoApiStub.SetupOAuthToken();
-        foreach (var prn in prns)
-            await WasteOrganisationsApiStub.WithOrganisation(
-                Guid.Parse(prn.IssuedToOrganisation!.Id!),
-                WoApiOrganisationType.ComplianceScheme
-            );
+        public static async Task SetupOrganisations(
+            List<PackagingRecyclingNote> prns,
+            CognitoApi cognitoApiStub,
+            WasteOrganisationsApi WasteOrganisationsApiStub
+        )
+        {
+            await cognitoApiStub.SetupOAuthToken();
+            foreach (var prn in prns)
+                await WasteOrganisationsApiStub.WithOrganisation(
+                    Guid.Parse(prn.IssuedToOrganisation!.Id!),
+                    WoApiOrganisationType.ComplianceScheme
+                );
+        }
     }
 }
