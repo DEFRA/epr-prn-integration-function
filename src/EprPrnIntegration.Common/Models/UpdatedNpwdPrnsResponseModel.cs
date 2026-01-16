3d3b3158d927030544fddfe37be5592a63734a77
Tim Peel <tim.peel@equalexperts.com>
2026-01-16 14:31:48 +0000

    Allow ObligationYear to be sent to NPWD if it's received from the EPR PRN API

--- src/EprPrnIntegration.Common/Models/UpdatedNpwdPrnsResponseModel.cs
+++ src/EprPrnIntegration.Common/Models/UpdatedNpwdPrnsResponseModel.cs
@@ -7,6 +7,9 @@ public class UpdatedNpwdPrnsResponseModel
 {
     public required string EvidenceNo { get; set; }
     public required string EvidenceStatusCode { get; set; }
-    public required string ObligationYear { get; set; }
+    /// <summary>
+    /// http://epr-prn-common-backend/api/v1/prn/ModifiedPrnsByDate endpoint does not return this field yet
+    /// </summary>
+    public string? ObligationYear { get; set; }
     public DateTime? StatusDate { get; set; }
 }
