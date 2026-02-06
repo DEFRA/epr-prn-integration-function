d2ee1091810c97c9b35c9fa5638aac844099c56f
Tim Peel <tim.peel@equalexperts.com>
2026-02-06 16:13:57 +0000

    Merge FunctionContext and FunctionExecutionContext

--- src/EprPrnIntegration.Api.IntegrationTests/FunctionContext.cs
+++ src/EprPrnIntegration.Api.IntegrationTests/FunctionContext.cs
@@ -1,16 +1,29 @@
 using System.Text;
+using Azure.Storage.Blobs;
+using EprPrnIntegration.Common.Helpers;
+using EprPrnIntegration.Common.Service;
 
 namespace EprPrnIntegration.Api.IntegrationTests;
 
 public static class FunctionContext
 {
     private static readonly HttpClient HttpClient;
+    private static readonly LastUpdateService LastUpdateService;
 
     static FunctionContext()
     {
         HttpClient = new HttpClient { BaseAddress = new Uri($"{BaseUri}/admin/functions/") };
-
         HttpClient.DefaultRequestHeaders.Add("x-functions-key", "this-is-a-dummy-value");
+        
+        // This connection string is the well-known default credential for the Azurite storage emulator.
+        // It is NOT a sensitive secret - it's a hardcoded value built into Azurite for local development.
+        // See: https://learn.microsoft.com/en-us/azure/storage/common/storage-use-azurite#well-known-storage-account-and-key
+        const string connectionString = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1;QueueEndpoint=http://localhost:10001/devstoreaccount1;TableEndpoint=http://localhost:10002/devstoreaccount1;";
+        
+        var blobServiceClient = new BlobServiceClient(connectionString);
+        var blobStorage = new BlobStorage(blobServiceClient);
+        
+        LastUpdateService = new LastUpdateService(blobStorage);
     }
 
     private static string BaseUri => "http://localhost:7234";
@@ -29,4 +42,6 @@ public static class FunctionContext
 
     public static async Task<HttpResponseMessage> Get(string requestUri) =>
         await HttpClient.GetAsync(requestUri);
+
+    public static Task<DateTime?> GetLastUpdate(string functionName) => LastUpdateService.GetLastUpdate(functionName);
 }
